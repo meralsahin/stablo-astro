1937d3f7b552185b6f25a64b580e9d407351ad99
Meral Sahin <meralsahinn@gmail.com>
2024-09-26 19:14:17 +0100

    Update

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -1,7 +1,3 @@
-import marioImage from "../assets/authors/mario.webp";
-import joshuaImage from "../assets/authors/joshua.webp";
-import erikaImage from "../assets/authors/erika.webp";
-
 export interface Props {
   name: string;
   slug: string;
@@ -13,21 +9,9 @@ export type Author = Props;
 
 export const authors: Props[] = [
   {
-    name: "Mario Sanchez",
-    slug: "mario-sanchez",
-    image: marioImage,
-    bio: "Mario is a Staff Engineer specialising in Frontend at Vercel, as well as being a co-founder of Acme and the content management system Sanity. Prior to this, he was a Senior Engineer at Apple.",
-  },
-  {
-    name: "Joshua Wood",
-    slug: "joshua-wood",
-    image: joshuaImage,
-    bio: "Joshua is a Microsoft Azure Certified Cloud Professional and a Google Certified Associate Cloud Engineer. A Data Analytics at Acme, specializing in the use of cloud infrastructure for Machine Learning and Deep Learning operation at scale.",
-  },
-  {
-    name: "Erika Oliver",
-    slug: "erika-oliver",
-    image: erikaImage,
-    bio: "Erika Oliver is a successful entrepreuner. She is the founder of Acme Inc, a bootstrapped business that builds affordable SaaS tools for local news, indie publishers, and other small businesses.",
-  },
+    name: "Merel Sahin",
+    slug: "meral-sahin",
+    image: "./src/assets/authors/italy.png",
+    bio: "",
+  }
 ];
