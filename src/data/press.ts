c9c2af1f17e3ee8053286b1b529d8c5d304b3fe7
Meral Sahin <meralsahinn@gmail.com>
2026-02-24 12:54:14 +0000

    Adding press links

---
+++ src/data/press.ts
@@ -0,0 +1,18 @@
+export interface PressItem {
+  title: string;
+  url: string;
+  image: string;
+}
+
+export const pressItems: PressItem[] = [
+  {
+    title: "Meral Şahin'in 'Freedom'ı Londra'da İlgi Gördü",
+    url: "https://www.avrupaajansi.com/meral-sahinin-freedomi-londrada-ilgi-gordu#",
+    image: "https://avrupaajansicom.teimg.com/crop/1280x720/avrupaajansi-com/uploads/2026/02/meral-sahin.JPG",
+  },
+  {
+    title: "8th Turkish Community Art Exhibition",
+    url: "https://yeelondon.org.uk/8thtcae/",
+    image: "https://yeelondon.org.uk/wp-content/uploads/2025/01/8thTCAE-1024x1024.jpg",
+  },
+];
